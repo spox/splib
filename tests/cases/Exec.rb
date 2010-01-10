a7ddabe62dd2ae81d8da0533783348c654823163
spox <spox@modspox.com>
2010-01-10 06:39:13 -0800

    update tests

--- tests/cases/Exec.rb
+++ tests/cases/Exec.rb
@@ -5,7 +5,7 @@ class ExecTest < Test::Unit::TestCase
     def setup
         Splib.load :Exec
     end
-    
+
     def test_exec
         assert_raise(IOError) do
             Splib.exec('echo test', 10, 1)
