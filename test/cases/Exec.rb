f23e646f68b6dec7cb52649e54067f59968c6d9a
spox <spox@modspox.com>
2010-01-29 06:54:11 -0800

    add test to check running processes are stored

--- test/cases/Exec.rb
+++ test/cases/Exec.rb
@@ -15,4 +15,16 @@ class ExecTest < Test::Unit::TestCase
         end
         assert_equal("test\n", Splib.exec('echo test'))
     end
+
+    def test_running
+        output = nil
+        Thread.new do
+            output = Splib.exec('ruby -e \'sleep(0.4); puts "done"\'')
+        end
+        sleep(0.1)
+        assert_equal(1, Splib.running_procs.size)
+        sleep(0.5)
+        assert_equal("done\n", output)
+        assert_equal(0, Splib.running_procs.size)
+    end
 end
