4e81af2ab6ae7438e507bd2c13bc8619bef6e716
spox <spox@modspox.com>
2010-03-28 08:26:23 -0700

    use generic shell everywhere

--- test/cases/Exec.rb
+++ test/cases/Exec.rb
@@ -8,18 +8,18 @@ class ExecTest < Test::Unit::TestCase
 
     def test_exec
         assert_raise(IOError) do
-            Splib.exec('echo test', 10, 1)
+            Splib.exec('/bin/sh -c "echo test"', 10, 1)
         end
         assert_raise(Timeout::Error) do
-            Splib.exec('while [ true ]; do true; done;', 1)
+            Splib.exec('/bin/sh -c "while [ true ]; do true; done;"', 1)
         end
-        assert_equal("test\n", Splib.exec('echo test'))
+        assert_equal("test\n", Splib.exec('/bin/sh -c "echo test"'))
     end
 
     def test_running
         output = nil
         Thread.new do
-            output = Splib.exec('/bin/sh -c "sleep 0.4; echo \\"done\\""')
+            output = Splib.exec('/bin/sh -c "sleep 0.4; echo done"')
         end
         sleep(0.1)
         assert_equal(1, Splib.running_procs.size)
