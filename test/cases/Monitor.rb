416fb52ef70c6eb6f37b6f068b27c1c232189cbb
spox <spox@modspox.com>
2010-04-06 15:18:02 -0700

    updated tests

--- test/cases/Monitor.rb
+++ test/cases/Monitor.rb
@@ -2,15 +2,16 @@ require 'splib'
 require 'test/unit'
 
 class MonitorTest < Test::Unit::TestCase
+    Splib.load :Monitor
+    Splib.load :Sleep
     def setup
-        Splib.load :Monitor
         @monitor = Splib::Monitor.new
     end
 
     def test_wait
         t = []
         5.times{ t << Thread.new{ @monitor.wait } }
-        sleep(0.01) # give threads a chance to wait
+        Splib.sleep(0.1) # give threads a chance to wait
         t.each do |thread|
             assert(thread.alive?)
             assert(thread.stop?)
@@ -19,141 +20,166 @@ class MonitorTest < Test::Unit::TestCase
 
     def test_wait_timeout
         t = []
-        o = []
-        5.times{|i| t << Thread.new{ @monitor.wait((i+1)/100.0); o << 1 } }
-        sleep(0.011)
-        Thread.pass
-        assert(!t.shift.alive?)
-        assert_equal(1, o.size)
-        sleep(0.1)
+        o = Queue.new
+        5.times{ t << Thread.new{ @monitor.wait(0.1); o << 1 } }
+        Splib.sleep(0.3)
         assert_equal(5, o.size)
-        t.each do |th|
-            assert(!th.alive?)
-        end
     end
 
     def test_signal
-        output = []
+        output = Queue.new
         t = []
         5.times{ t << Thread.new{ @monitor.wait; output << 1 } }
-        sleep(0.01)
+        Splib.sleep(0.01)
         t.each do |thread|
             assert(thread.alive?)
             assert(thread.stop?)
         end
         assert(output.empty?)
         @monitor.signal
-        sleep(0.01)
+        Splib.sleep(0.01)
         assert_equal(4, t.select{|th|th.alive?}.size)
         assert_equal(1, output.size)
         assert_equal(1, output.pop)
     end
 
     def test_broadcast
-        output = []
+        output = Queue.new
         t = []
         5.times{ t << Thread.new{ @monitor.wait; output << 1 } }
-        sleep(0.01)
+        Splib.sleep(0.01)
         t.each do |thread|
             assert(thread.alive?)
             assert(thread.stop?)
         end
+        assert_equal(5, t.size)
         assert(output.empty?)
         @monitor.broadcast
-        sleep(0.01)
+        Splib.sleep(0.1)
         assert_equal(5, output.size)
         5.times{ assert_equal(1, output.pop) }
         assert_equal(5, t.select{|th|!th.alive?}.size)
     end
 
+    def test_synchronize_broadcast
+        output = Queue.new
+        t = []
+        5.times{ t << Thread.new{ @monitor.wait; output << 1 } }
+        Splib.sleep(0.01)
+        t.each do |thread|
+            assert(thread.alive?)
+            assert(thread.stop?)
+        end
+        assert_equal(5, t.size)
+        assert(output.empty?)
+        @monitor.synchronize{ @monitor.broadcast }
+        Splib.sleep(0.1)
+        assert_equal(5, output.size)
+        5.times{ assert_equal(1, output.pop) }
+        assert_equal(5, t.select{|th|!th.alive?}.size)
+    end
+    
+    def test_multi_lock
+        @monitor.synchronize do
+            @monitor.synchronize do
+                @monitor.lock
+                @monitor.synchronize do
+                    true
+                end
+                @monitor.unlock
+            end
+        end
+    end
+
     def test_wait_while
         stop = true
         t = Thread.new{ @monitor.wait_while{ stop } }
-        sleep(0.01)
+        Splib.sleep(0.01)
         assert(t.alive?)
         assert(t.stop?)
         @monitor.signal
-        sleep(0.01)
+        Splib.sleep(0.01)
         assert(t.alive?)
         assert(t.stop?)
         @monitor.broadcast
-        sleep(0.01)
+        Splib.sleep(0.01)
         assert(t.alive?)
         assert(t.stop?)
         stop = false
         @monitor.signal
-        sleep(0.01)
+        Splib.sleep(0.01)
         assert(!t.alive?)
         assert(t.stop?)
     end
 
     def test_wait_until
         stop = false
-        t = Thread.new{ @monitor.wait_until{ stop } }
-        sleep(0.01)
+        done = false
+        t = Thread.new{ @monitor.wait_until{ stop }; done = true; }
+        Splib.sleep(0.01)
         assert(t.alive?)
         assert(t.stop?)
         @monitor.signal
-        sleep(0.01)
+        Splib.sleep(0.01)
         assert(t.alive?)
         assert(t.stop?)
         @monitor.broadcast
-        sleep(0.01)
+        Splib.sleep(0.01)
         assert(t.alive?)
         assert(t.stop?)
         stop = true
         @monitor.signal
-        sleep(0.01)
-        assert(!t.alive?)
-        assert(t.stop?)
+        Splib.sleep(0.1)
+        assert(done)
     end
 
     def test_waiters
         t = []
         (rand(20)+1).times{ t << Thread.new{ @monitor.wait } }
-        sleep(0.01)
+        Splib.sleep(0.1)
         assert_equal(t.size, @monitor.waiters)
         @monitor.broadcast
-        sleep(0.01)
+        Splib.sleep(0.1)
         assert_equal(0, @monitor.waiters)
     end
 
     def test_lock_unlock
         t = []
         output = []
-        3.times{|i| t << Thread.new{ @monitor.lock; sleep((i+1)/100.0); output << i; @monitor.unlock;}}
-        sleep(0.011)
+        3.times{|i| t << Thread.new{ @monitor.lock; Splib.sleep(0.1); output << i; @monitor.unlock;}}
+        Splib.sleep(0.12)
         assert_equal(1, output.size)
-        sleep(0.021)
+        Splib.sleep(0.12)
         assert_equal(2, output.size)
-        sleep(0.031)
+        Splib.sleep(0.12)
         assert_equal(3, output.size)
         assert(!t.any?{|th|th.alive?})
         3.times{|i|assert_equal(i, output.shift)}
     end
-    
+
     def test_lock_unlock_wakeup
         complete = false
-        t1 = Thread.new{@monitor.lock; sleep(0.1); @monitor.unlock}
+        t1 = Thread.new{@monitor.lock; Splib.sleep(0.1); @monitor.unlock}
+        Splib.sleep(0.01)
         t2 = Thread.new{@monitor.lock; complete = true; @monitor.unlock}
         assert(!complete)
         assert(t1.alive?)
         t2.wakeup
         Thread.pass
         assert(!complete)
-        sleep(0.11)
+        Splib.sleep(0.15)
         assert(complete)
     end
 
     def synchronize
         t = []
         output = []
-        5.times{|i| t << Thread.new{ @monitor.lock; sleep(i/100.0); output << i; @monitor.unlock;}}
+        5.times{|i| t << Thread.new{ @monitor.lock; Splib.sleep(i/100.0); output << i; @monitor.unlock;}}
         @monitor.synchronize{ output << :done }
-        sleep(0.5)
+        Splib.sleep(0.5)
         assert_equal(6, output.size)
     end
-    
+
     def test_try_lock
         assert(@monitor.try_lock)
         assert(@monitor.locked?)
