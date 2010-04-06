43cf7466fa9dbb3767f0303803dec3da821abc21
spox <spox@modspox.com>
2010-04-06 15:16:25 -0700

    times end up being the same. use default but use the better return of actual sleep

--- lib/splib/Sleep.rb
+++ lib/splib/Sleep.rb
@@ -1,18 +1,9 @@
 module Splib
 class << self
-    # secs:: Number of seconds to sleep (Use float to provide msec sleep)
-    # Modified sleep method to use select() to provide msec level
-    # sleep times
+    # secs:: Number of seconds to sleep (Use float to provide better actual sleep time)
     def sleep(secs=nil)
-        unless(secs.nil?)
-            begin
-                secs = secs.to_f
-            rescue NoMethodError
-                raise TypeError.new("can't convert #{secs.class} into time interval")
-            end
-        end
         start = Time.now.to_f
-        select(nil, nil, nil, secs)
+        secs.nil? ? Kernel.sleep : Kernel.sleep(secs)
         Time.now.to_f - start
     end
 end
