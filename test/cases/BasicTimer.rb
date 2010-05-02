a0f761c7a5d363f7d4cc39653aaa494dbb1dbf58
spox <spox@modspox.com>
2010-05-02 07:54:48 -0700

    add test for BasicTimer

---
+++ test/cases/BasicTimer.rb
@@ -0,0 +1,43 @@
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'splib/BasicTimer'
+require 'thread'
+
+class BasicTimerTest < Test::Unit::TestCase
+    def setup
+        @timer = Splib::BasicTimer.new
+    end
+
+    def test_add_single
+        output = Queue.new
+        b = @timer.add(:period => 0.1){ output << :foo }
+        assert_kind_of(Proc, b)
+        sleep(1)
+        assert_equal(9, output.size)
+    end
+
+    def test_remove_single
+        output = Queue.new
+        b = @timer.add(:period => 0.1){ output << :foo }
+        assert_kind_of(Proc, b)
+        sleep(0.11)
+        assert_equal(1, output.size)
+        @timer.remove(b)
+        sleep(0.1)
+        assert_equal(1, output.size)
+    end
+
+    def test_add_multiple
+        output = Queue.new
+        @timer.add(:period => 0.1){ output << :foo }
+        @timer.add(:period => 0.2){ output << :fee }
+        @timer.add(:period => 0.5){ output << :fubar }
+        sleep(0.51)
+        assert_equal(7, output.size)
+    end
+
+    def test_remove_multiple
+        
+    end
+end
