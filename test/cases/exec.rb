15f0d5dac765c43280289831b65820125ed78bab
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-21 10:37:42 -0700

    Don't cause sub-process (need to add child tracking later)

--- test/cases/exec.rb
+++ test/cases/exec.rb
@@ -8,18 +8,18 @@ class ExecTest < Test::Unit::TestCase
   end
   def test_exec
     assert_raise(IOError) do
-      Splib.exec('/bin/sh -c "echo test"', 10, 1)
+      Splib.exec('echo test', 10, 1)
     end
     assert_raise(Timeout::Error) do
-      Splib.exec('/bin/sh -c "while [ true ]; do true; done;"', 1)
+      Splib.exec('while [ true ]; do true; done;', 1)
     end
-    assert_equal("test\n", Splib.exec('/bin/sh -c "echo test"'))
+    assert_equal("test\n", Splib.exec('echo test'))
   end
 
   def test_running
     output = nil
     Thread.new do
-      output = Splib.exec('/bin/sh -c "sleep 0.4; echo done"')
+      output = Splib.exec('sleep 0.4; echo done')
     end
     Kernel.sleep(0.1)
     assert_equal(1, Splib.running_procs.size)
